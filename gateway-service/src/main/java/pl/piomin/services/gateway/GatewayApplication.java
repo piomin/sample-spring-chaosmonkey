4a73ed614f4b0f064ff19a63f0ba5e4c1a17d4fe
piomin <piotr.minkowski@gmail.com>
2023-07-25 13:04:40 +0200

    fix errors

--- gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
@@ -2,12 +2,8 @@ package pl.piomin.services.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
--- order-service/src/main/java/pl/piomin/services/order/model/Order.java
+++ order-service/src/main/java/pl/piomin/services/order/model/Order.java
@@ -1,10 +1,10 @@
 package pl.piomin.services.order.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import org.springframework.data.annotation.Transient;
 
